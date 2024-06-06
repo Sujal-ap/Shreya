596f0f77dee5c7d1baf871bbcd7f557e37390979
Sujal-ap <ushabalmiki8@gmail.com>
2024-06-06 16:49:03 +0530

    hghguh

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import '../style.css';  // Import the CSS file
+import '../styles.css';  // Import the CSS file
 
 const Banner = () => {
   const settings = {
