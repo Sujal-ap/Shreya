9f42adcc41ed635ec3fa26238d6d47bdf62d41a3
Sujal-ap <ushabalmiki8@gmail.com>
2024-06-06 14:59:40 +0530

    bannerbg

---
+++ src/components/Banner.js
@@ -0,0 +1,16 @@
+// src/components/Banner.js
+import React from 'react';
+import '../style.css';  // Import the CSS file
+
+const Banner = () => {
+  return (
+    <div className="banner-wrapper">
+      <div className="banner-content">
+        <h1>Welcome to Vintage Thrift Store</h1>
+        <p>Discover unique vintage clothing and accessories</p>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
