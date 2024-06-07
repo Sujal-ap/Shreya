59db94b69346c2471ebfff179e1a49afa44f708c
Sujal-ap <ushabalmiki8@gmail.com>
2024-06-07 11:22:58 +0530

    lgffhhfdrd

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,8 +1,31 @@
-// src/components/ImageGrid.js
-import React from 'react';
-import '../grid.css'; // Ensure the styles are applied
+import React, { useEffect } from 'react';
+import '../grid.css';  // Ensure the styles are applied
 
-const ImageGrid = () => {
+const Grid = () => {
+  useEffect(() => {
+    const gridItems = document.querySelectorAll('.image-grid-item');
+
+    gridItems.forEach(item => {
+      item.addEventListener('touchstart', handleTouchStart);
+      item.addEventListener('touchend', handleTouchEnd);
+    });
+
+    return () => {
+      gridItems.forEach(item => {
+        item.removeEventListener('touchstart', handleTouchStart);
+        item.removeEventListener('touchend', handleTouchEnd);
+      });
+    };
+  }, []);
+
+  const handleTouchStart = (e) => {
+    e.currentTarget.classList.add('hovered');
+  };
+
+  const handleTouchEnd = (e) => {
+    e.currentTarget.classList.remove('hovered');
+  };
+
   return (
     <div className="image-grid">
       {Array.from({ length: 12 }, (_, index) => (
@@ -12,4 +35,4 @@ const ImageGrid = () => {
   );
 };
 
-export default ImageGrid;
+export default Grid;
