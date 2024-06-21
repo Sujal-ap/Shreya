62c61092bfafd42c7735127526bf8d9e9ceb5e28
Sujal-ap <ushabalmiki8@gmail.com>
2024-06-21 12:52:37 +0530

    hhfffdhhg

--- src/components/SignupButton.js
+++ src/components/SignupButton.js
@@ -2,35 +2,35 @@
 
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { useAuth } from '../components/AuthContext.js'; // Import your authentication context
 import '../styles/signupb.css';
 
 const SignupButton = () => {
+  const { user } = useAuth(); // Assuming useAuth() provides user authentication state
   const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
-      // Calculate header height or any other relevant threshold
       const headerHeight = document.querySelector('header').offsetHeight;
       
-      // Determine scroll position
-      if (window.scrollY > headerHeight) {
-        setIsVisible(true); // Show signup button
+      if (!user && window.scrollY > headerHeight) {
+        setIsVisible(true); // Show signup button only if user is not signed in
       } else {
-        setIsVisible(false); // Hide signup button
+        setIsVisible(false); // Hide signup button if user is signed in or scroll position is above header
       }
     };
 
-    // Add scroll event listener
     window.addEventListener('scroll', handleScroll);
 
-    // Clean up scroll event listener
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
-  }, []);
+  }, [user]); // Add user to dependency array to listen for changes
+
+  if (user) return null; // If user is signed in, do not render the signup button
 
   return (
-    <Link to="/Signup" className={`signup-button ${isVisible ? 'visible' : 'hidden'}`}>
+    <Link to="/Signup" className={`signup-button ${isVisible ? 'visible' : ''}`}>
       <span>→</span> Sign Up
     </Link>
   );
