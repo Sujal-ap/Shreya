027b31da1a8694d493a51fd96aa4db1b2e6cffbb
Sujal-ap <ushabalmiki8@gmail.com>
2024-06-22 00:39:03 +0530

    jhgfgjhj

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,12 +1,26 @@
-// src/pages/SignUp.js
 import React from 'react';
-import SignUpPage from '../components/Signuppage.js'; // Import the corrected SignUpPage component
+import SignUpPage from '../components/Signuppage.js';
 import '../styles/signup.css';
 
 const Signup = () => {
   return (
     <div className="signup-wrapper">
-      <SignUpPage /> {/* Render SignUpPage component directly */}
+      <SignUpPage />
+      <div className="background-bars">
+        {/* White bars */}
+        <div className="bar white" style={{ left: '0%' }}></div>
+        <div className="bar white" style={{ left: '20%' }}></div>
+        <div className="bar white" style={{ left: '40%' }}></div>
+        <div className="bar white" style={{ left: '60%' }}></div>
+        <div className="bar white" style={{ left: '80%' }}></div>
+        
+        {/* Black bars */}
+        <div className="bar black" style={{ left: '10%' }}></div>
+        <div className="bar black" style={{ left: '30%' }}></div>
+        <div className="bar black" style={{ left: '50%' }}></div>
+        <div className="bar black" style={{ left: '70%' }}></div>
+        <div className="bar black" style={{ left: '90%' }}></div>
+      </div>
     </div>
   );
 }
